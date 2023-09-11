a0c4f6d233c7e937eb505bddbce67f7801a9e682
Daniel Sobolev <danielsobolev@yahoo.com>
2023-09-10 20:39:20 -0400

    add string to money format

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,14 @@
 export const StringFormat = (str: string, ...args: string[]) => {
     return str.replace(/{(\d+)}/g, (match, index) => args[index] || '')
 }
+
+export const MoneyFormat = (price: number) => {
+    // Format the price above to USD using the locale, style, and currency.
+    // Can be changed to use the 
+    const Dollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    return Dollar.format(price)
+}
