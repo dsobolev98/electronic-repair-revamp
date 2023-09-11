3f94853cbe89509de531ffeb3da30fc6354e3855
Daniel Sobolev <danielsobolev@yahoo.com>
2023-09-10 20:35:15 -0400

    add admin functionality to edit part and labor info

--- src/components/admin/item-invoice/ItemInvoice.tsx
+++ src/components/admin/item-invoice/ItemInvoice.tsx
@@ -1,50 +1,116 @@
-"use server"
+'use client'
 
-import { ItemInfo } from '@/models/InquiryData'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './item-invoice.module.css'
-import {HTMLAttributes} from '@/app/admin/[id]/attributes'
+import { ItemInfo, PartType } from '@/models/InquiryData'
+import { HTMLAttributes } from '@/app/admin/[id]/attributes'
+import { event } from 'jquery'
 
-async function ItemInvoice({
-    items
+export default function ItemInvoice({
+    itemsProp
 }:{
-    items: Array<ItemInfo>
+    itemsProp: string
 }) {
-  return (
-    <div>
-        <h3>Items: </h3>
-        {
-            items.map((item, index) => 
-                <div key={index}>
-                    <h4>{item.brand} {item.model}</h4>
-                    <div className={styles.row}>
-                        <div className={styles.left}>
-                            <p>Parts:</p>
-                        </div>
-                        <div className={styles.right}>
-                            <input 
-                                placeholder={item.Part[0]?.Price?.toString() ?? '0.00'} 
-                                name={index + HTMLAttributes.PartPrice + '0'} 
-                            />
-                        </div>
-                    </div>
+    const [items, setItems] = useState(JSON.parse(itemsProp) as Array<ItemInfo>)
 
-                    <div className={styles.row}>
-                        <div className={styles.left}>
-                            <p>Labor:</p>
+    function addPart(itemIndex: number) {
+        console.log('Trying to add new part for item '+ itemIndex)
+        const newPart = {
+            Name: '',
+            Price: 0.00
+        } as PartType
+        var newItems = items.slice()
+        if (items[itemIndex].Part.length < 8)
+            newItems[itemIndex].Part.push(newPart)
+        setItems(newItems)
+        console.log('Finished adding new part to state')
+        console.log(items)
+    }
+
+    function deletePart(itemIndex: number, partIndex: number) {
+        console.log('Delete item ' + itemIndex + ' on part ' + partIndex)
+
+        var newPartArray: PartType[] = []
+        items[itemIndex].Part.forEach((part, index) => {
+            if (partIndex != index) {
+                newPartArray.push(part)
+            }
+        })
+        console.log(newPartArray)
+
+        var newItems = items.slice()
+        newItems[itemIndex].Part = newPartArray
+        setItems(newItems)
+    }
+
+    return (
+        <div>
+            <h3>Items: </h3>
+            {
+                items.map((item, itemIndex) => 
+                    <div key={'item'+itemIndex}>
+                        <h4>{item.brand} {item.model}</h4>
+                        <div className={styles.row}>
+                            <div className={styles.left}>
+                                <p>Parts:</p>
+                            </div>
+                            <div className={styles.right}>
+                                {
+                                    item.Part.map((part, partIndex) => 
+                                        <div key={'part'+partIndex} className={styles.row}>
+                                            <label> &nbsp; Part Name: &nbsp; </label>
+                                            <input 
+                                                defaultValue={ part.Name?.toString() ?? '' }
+                                                readOnly={part.Name ? true : false }
+                                                name={itemIndex + HTMLAttributes.PartName + partIndex}
+                                            />
+                                            <label> &nbsp; Price: &nbsp; </label>
+                                            <input 
+                                                defaultValue={ part.Price > 0 ? part.Price.toString() : '' }
+                                                readOnly={part.Price > 0 ? true : false }
+                                                name={itemIndex + HTMLAttributes.PartPrice + partIndex}
+                                            />
+                                            <button 
+                                                type='button' 
+                                                onClick={(event: any) => deletePart(itemIndex, partIndex)}
+                                                disabled={item.Part.length > 1 ? false : true}
+                                            > Delete </button>
+                                        </div>
+                                    )
+                                }
+                                <div className={`${styles.row}`}>
+                                    <div className={styles.right}>
+                                    <button 
+                                        type='button' 
+                                        onClick={(event:any) => addPart(itemIndex)}
+                                        disabled={(item?.Part?.length ?? 0) < 8 ? false : true}
+                                    > Add Part </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.right}>
-                            <input 
-                                placeholder={item?.Labor?.Price.toString() ?? '0.00'} 
-                                name={index + HTMLAttributes.LaborPrice}
-                            />
+
+                        <div className={styles.row}>
+                            <div className={styles.left}>
+                                <p>Labor:</p>
+                            </div>
+                            <div className={styles.right}>
+                                <label>Hour(s):</label>
+                                <input 
+                                    defaultValue={item?.Labor?.Hours.toString() ?? '0'}
+                                    name={itemIndex + HTMLAttributes.LaborHours}
+                                />
+
+                                <label>Price:</label>
+                                <input 
+                                    defaultValue={item?.Labor?.Price.toString() ?? '0.00'} 
+                                    name={itemIndex + HTMLAttributes.LaborPrice}
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-            )
-        }
-    </div>
-  )
+                )
+            }
+        </div>
+    )
 }
-
-export default ItemInvoice
